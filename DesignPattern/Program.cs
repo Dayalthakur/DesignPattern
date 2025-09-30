662192089da951ec47250782db85c0c0e094601f
Dayal Thakur <thakurdaya2001@gmail.com>
2025-09-30 23:07:44 +0530

    Add project files.

---
+++ DesignPattern/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace DesignPattern
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //Console.WriteLine("No of stars for Right-Angle Triangle Pattern : ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("* ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.WriteLine("\n");
+
+            //Console.WriteLine("No of stars for Inverted Right-Angle Triangle Pattern : ");
+            //int n1 = Convert.ToInt32(Console.ReadLine());
+            //for(int i = n1; i >= 1; i--)
+            //{
+            //    for(int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("* ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.WriteLine("\n");
+
+        }
+    }
+}
