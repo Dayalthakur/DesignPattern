b89684c207ef0ca735a5c76a7374aa1726dcf6c6
Dayal Thakur <thakurdaya2001@gmail.com>
2025-10-02 13:54:51 +0530

    Last Commit

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -17,11 +17,13 @@
             //}
             //Console.WriteLine("\n");
 
+
+
             //Console.WriteLine("No of stars for Inverted Right-Angle Triangle Pattern : ");
             //int n1 = Convert.ToInt32(Console.ReadLine());
-            //for(int i = n1; i >= 1; i--)
+            //for (int i = n1; i >= 1; i--)
             //{
-            //    for(int j = 1; j <= i; j++)
+            //    for (int j = 1; j <= i; j++)
             //    {
             //        Console.Write("* ");
             //    }
@@ -29,6 +31,50 @@
             //}
             //Console.WriteLine("\n");
 
+
+            //Console.WriteLine("No. of rows for pyramid pattern : ");
+            //int n2 = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= n2; i++)
+            //{
+            //    for (int space = i; space < n2; space++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("* ");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+
+
+            Console.WriteLine("Diamond Pattern");
+            int n3 = Convert.ToInt32(Console.ReadLine());
+            for(int i = 1; i <= n3; i++)
+            {
+                for(int space = i; space < n3; space++)
+                {
+                    Console.Write(" ");
+                }
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                } 
+                Console.WriteLine();
+            }
+            for (int i=n3-1;i>=1;i--)
+            {
+                for(int space = i; space < n3; space++)
+                {
+                    Console.Write(" ");
+                }
+                for(int j= 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
